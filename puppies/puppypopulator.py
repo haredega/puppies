02af3774a08a32454d6241035be82884cdba6400
haredega <hgarcia.ee@gmail.com>
2016-02-21 15:19:31 -0300

    Corrects database URL into puppypopulator.py

--- puppies/puppypopulator.py
+++ puppies/puppypopulator.py
@@ -8,7 +8,7 @@ import datetime
 import random
 
 
-engine = create_engine('sqlite:///puppyshelter.db')
+engine = create_engine('postgres://gtjypbmogjtjbr:GKcpbGlzrLj9JlXR-S03kTXup4@ec2-107-20-148-211.compute-1.amazonaws.com:5432/d78btdqq3a0f69')
 
 Base.metadata.bind = engine
 
