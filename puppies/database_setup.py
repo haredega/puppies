6b5a48e2fd09960a990c5c2edb9371953d2c8c08
haredega <hgarcia.ee@gmail.com>
2016-02-21 15:31:25 -0300

    Imports os

--- puppies/database_setup.py
+++ puppies/database_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
 Base = declarative_base()
 
